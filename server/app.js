e8411f9572b280409a5a2b56d74b46a4ed6ef31a
Aleksey Chebykin <chebykinapwork@gmail.com>
2022-09-30 14:39:37 +0300

    add some fixes in server app.js

--- server/app.js
+++ server/app.js
@@ -6,10 +6,10 @@ require('dotenv').config();
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../build')));
+  app.use(express.static(path.join(__dirname + 'client/build')));
 
   app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
 }
 
