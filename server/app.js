6ddf0996ceaf094684138a813c46f7da569ff31f
Aleksey Chebykin <chebykinapwork@gmail.com>
2022-10-01 15:10:00 +0300

    add change in server/app.js

--- server/app.js
+++ server/app.js
@@ -6,10 +6,10 @@ require('dotenv').config();
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname + '../build')));
+  app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '..build/index.html'));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
 
